656ff7cacf78d4ad21456959d5b4be8287157717
Austin <Austin@AUSTIN-LENOVAMONSTER.telus>
2017-01-04 15:11:22 -0700

    Basic singleton class created.

---
+++ src/singletonPatternTest/Singleton.java
@@ -0,0 +1,18 @@
+package singletonPatternTest;
+
+
+public class Singleton {
+	private static Singleton instance = null;
+	
+	//this is here just to handle instantiation attempts
+	protected Singleton(){
+	}
+	
+	public static Singleton getInstance(){
+		if(instance == null){
+			instance = new Singleton();
+		}
+		return instance;
+	}
+	
+}
